08ccb9d5adbd90116c5129050286d7b55f952ca7
sinsk23 <sinsk23@naver.com>
2022-09-09 16:36:30 +0900

    fix : js file

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
+const http = require("http");
 const app = express();
 const port = 3666;
 
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello Jenkins!");
   });
 
-
-app.listen(port, () => {
+http.createServer(app).listen(port, () => {
     console.log(port, "포트로 서버가 열렸어요!");
 });
+
+module.exports = app;
