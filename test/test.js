d3c48547f09eb6a606653be4e518b03724908642
sinsk23 <sinsk23@naver.com>
2022-09-16 19:51:43 +0900

    update test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request= require('supertest');
 const app = require('../app.js');
 
 describe('GET /', () => {
-    it('Hello Jenkins!! 요청와야함함!plz!.', () =>{
+    it('Hello Jenkins!! 요청와야함함!plz!plz.', () =>{
         //navigate to root and check the response is 'Hello Jenkins!'
         request(app).get('/').expect('Hello Jenkins!!')
     })
