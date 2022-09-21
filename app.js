a0fd429e23ab0caf9dcb435ee803f49996b2dedf
sinsk23 <sinsk23@naver.com>
2022-09-21 22:17:31 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require("http");
 const app = express();
 const PORT = process.env.PORT || 3666;
-// const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || 'localhost';
 const cors = require('cors');
    
 app.use(
@@ -26,11 +26,11 @@ app.get("/api",(req,res)=> {
 });
 });
 
-http.createServer(app).listen(PORT, () => {
-    console.log(`서버가  :${PORT}로 켜졌습니다`);
-});
-// http.createServer(app).listen(PORT,HOST, () => {
-//   console.log(`서버가  ${HOST}:${PORT}로 켜짐`);
+// http.createServer(app).listen(PORT, () => {
+//     console.log(`서버가  :${PORT}로 켜졌습니다`);
 // });
+http.createServer(app).listen(PORT,HOST, () => {
+  console.log(`서버가  ${HOST}:${PORT}로 켜짐`);
+});
 
 module.exports = app;
