82d327ac3d70ae69ff69e10e7023cf1e4679679b
sinsk23 <sinsk23@naver.com>
2022-09-21 14:32:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
   });
 
 http.createServer(app).listen(PORT,HOST, () => {
-    console.log(`서버가  ${HOST}:${PORT}로 켜짐`);
+    console.log(`서버  ${HOST}:${PORT} 켜짐`);
 });
 
 module.exports = app;
