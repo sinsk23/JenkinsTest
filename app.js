dd7fec5efc8718f8382d4b261bc4b6dced26946c
sinsk23 <sinsk23@naver.com>
2022-09-21 16:33:14 +0900

    feat : ejs 재추가

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ const http = require("http");
 const app = express();
 const PORT = process.env.PORT || 3666;
 const HOST = process.env.HOST || 'localhost';
-
+const cors = require('cors');
+   
+app.use(
+    cors({
+      origin: true, // 출처 허용 옵션
+      withCredentials: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+    })
+  );
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.get("/", (req, res) => {
     res.send("Hello Jenkins!!");
     
   });
+
+app.get("/api",(req,res)=> {
+  res.render('index',{
+  title: 'Hi Node Web Server!'
+});
+});
 
 http.createServer(app).listen(PORT,HOST, () => {
     console.log(`서버가  ${HOST}:${PORT}로 켜짐`);
